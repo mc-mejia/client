42fb5dfe1d01560a8c4d068ca09e761454cbaf34
Maria Mejia <mejia.maria1998@gmail.com>
2022-06-16 08:29:04 -0500

    Updated CreateProfile & register model
    Also updated their corresponding forms

--- src/app/profile/model/createprofile.ts
+++ src/app/profile/model/createprofile.ts
@@ -5,29 +5,32 @@ import { Experience } from "./experience";
 
 export class CreateProfile {
 
-    company: string;
-    website: string;
-    location: string;
-    skills: string;
-    githubusername: string;
-    bio: string;
-    twitter: string;
-    facebook: string;
-    youtube: string;
-    linkedin: string;
-    instagram: string;
-    experience: Experience[];
-    education: Education[];
+    // company: string;
+    // website: string;
+    // location: string;
+    // skills: string;
+    // githubusername: string;
+    // bio: string;
+    // twitter: string;
+    // facebook: string;
+    // youtube: string;
+    // linkedin: string;
+    // instagram: string;
+    // experience: Experience[];
+    // education: Education[];
  
     dob:string;
     handle: string;
     question: string;
     answer: string;
     ssn: string;
-    mobile: string;
+    phoneNo: string;
     status: string;
     banks: Bank[];
     ben: Beneficiary[];
     user: any;
     approved:boolean = true;
+    email: string;
+    password: string;
+    password2: string;
   }
