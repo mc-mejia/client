852e2c646f8e2e51fe127d21a35c872ae7822f23
Syawadhilah Pradipta <dipta0988@gmail.com>
2022-06-16 09:52:25 -0700

    finish view-banks

--- src/app/admin/components/view-banks/view-banks.component.ts
+++ src/app/admin/components/view-banks/view-banks.component.ts
@@ -1,34 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProfileService } from 'src/app/profile/services/profile.service';
-
+import { StaffService } from 'src/app/users/services/staff.service';
 @Component({
   selector: 'app-view-banks',
   templateUrl: './view-banks.component.html',
-  styleUrls: ['./view-banks.component.css']
+  styleUrls: ['./view-banks.component.css'],
 })
 export class ViewBanksComponent implements OnInit {
   //need: to update service calls
-  banks: any = {}
+  banks: any = {};
 
-  constructor(private profileService: ProfileService, private router: Router) { }
+  constructor(
+    private profileService: ProfileService,
+    private router: Router,
+    private staffService: StaffService
+  ) {}
 
   ngOnInit(): void {
-    this.profileService.getBanks().subscribe(
-      (res) =>{
+    this.staffService.getUnapprovedBankAccounts().subscribe(
+      (res) => {
         console.log(JSON.stringify(res));
         this.banks = res;
         console.log(JSON.stringify(this.banks));
       },
-      (err) =>{
+      (err) => {
         console.log(err.console.error());
-        
       }
     );
   }
 
-  toggleBank(id: any){
-
+  toggleBank(bankAccount: any) {
+    this.staffService.approveBankAccount(bankAccount).subscribe(
+      (res) => {
+        console.log(JSON.stringify(res));
+        this.banks = res;
+        console.log(JSON.stringify(this.banks));
+      },
+      (err) => {
+        console.log(err.console.error());
+      }
+    );
   }
-
 }
